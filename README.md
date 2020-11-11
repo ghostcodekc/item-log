4f33a3f700de74898a90af089a058360870f0166
GhostCodeKC <andrewmgrube@gmail.com>
2020-11-11 10:49:35 -0600

    Added Auth0 Login

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # item-log
- Home Item Log Application
+
+Home Item Log Application
+
+https://jamesqquick.hashnode.dev/the-easiest-way-to-add-nodejs-user-authentication
--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const { auth, requiresAuth } = require('express-openid-connect');
+
+app.use(
+    auth({
+        authRequired: false,
+        auth0Logout: true,
+        issuerBaseURL: process.env.ISSUER_BASE_URL,
+        baseURL: process.env.BASE_URL,
+        clientID: process.env.CLIENT_ID,
+        secret: process.env.SECRET,
+    })
+);
+
 
 app.get('/', (req, res) => {
-    res.send("hello world");
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
 });
 
 const port = process.env.PORT || 3000;
